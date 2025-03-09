958880aa559aa6773b0fe4c2cd811b7d98f8b206
Herman Semenov <GermanAizek@yandex.ru>
2025-03-10 02:52:51 +0300

    Added modprobe netif and env flag suppress MODPOST errors

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-cp -v /lib/modules/$(uname -r)/build/Module.symvers src/os/linux
-make -j$(nproc --all) clean && make -j$(nproc --all) all 
-rm -rvf src/os/linux/Module.symvers
-modprobe --dump-modversions src/os/linux/mtnet7601Usta.ko
+make -j$(nproc --all) clean
+
+cp -vf /usr/src/linux-headers-$(uname -r)/Module.symvers src/os/linux
+KBUILD_MODPOST_WARN=1 make -j$(nproc --all) all 
+
 modprobe --dump-modversions src/os/linux/mtutil7601Usta.ko
 modprobe --dump-modversions src/os/linux/mt7601Usta.ko
+modprobe --dump-modversions src/os/linux/mtnet7601Usta.ko
